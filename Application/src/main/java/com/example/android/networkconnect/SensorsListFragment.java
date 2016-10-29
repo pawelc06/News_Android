e24013ecc2b6cc1f88953ff9d4d6d15b75654615
Pawel Czop <pawel.czop@gmail.com>
2016-10-29 18:44:18 +0200

    added vbat0 reading, as added on STM32 server plus changfed sleep time for refresh to 60 s

--- Application/src/main/java/com/example/android/networkconnect/SensorsListFragment.java
+++ Application/src/main/java/com/example/android/networkconnect/SensorsListFragment.java
@@ -45,7 +45,7 @@ public class SensorsListFragment extends ListFragment implements AdapterView.OnI
 
                 while (appRunning) {
                     try {
-                        Thread.sleep(10000);
+                        Thread.sleep(60000);
                     }
                     catch (InterruptedException e) {
                         e.printStackTrace();
